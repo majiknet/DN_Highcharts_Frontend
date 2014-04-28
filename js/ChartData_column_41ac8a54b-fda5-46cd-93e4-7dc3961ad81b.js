8898f7094a6ea0713937c7c664f4938e59efe9f7
Mikael Kaulanen <mikael@majik.se>
2014-04-28 13:03:55 +0200

    Deleted files

--- js/ChartData_column_41ac8a54b-fda5-46cd-93e4-7dc3961ad81b.js
+++
@@ -1,2 +0,0 @@
-var testData =
-[{ "name": "(1) - (2)", "data": [10.60] }, { "name": "C", "data": [5.20] }, { "name": "FP", "data": [6.20] }, { "name": "KD", "data": [3.80] }, { "name": "M", "data": [37.50] }, { "name": "M FP C KD (1)", "data": [52.70] }, { "name": "MP", "data": [9.10] }, { "name": "S", "data": [27.70] }, { "name": "S V MP (2)", "data": [42.10] }, { "name": "SD", "data": [4.30] }, { "name": "V", "data": [5.30] }, { "name": "Övr, inkl SD", "data": [5.10] }]
